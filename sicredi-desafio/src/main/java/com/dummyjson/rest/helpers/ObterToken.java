e69d1ed86fa10d608f75d19c76382eceacc7428e
Leandro Pereira <leandro.nares@gmail.com>
2024-08-22 11:03:24 -0300

    Implementação da classe ObterToken

---
+++ sicredi-desafio/src/main/java/com/dummyjson/rest/helpers/ObterToken.java
@@ -0,0 +1,25 @@
+package com.dummyjson.rest.helpers;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ObterToken {
+	 public static String gerarToken(String username, String password) {
+	        Map<String, String> login = new HashMap<String, String>();
+	        login.put("username", username);
+	        login.put("password", password);
+
+	        return given()
+	                    .body(login)
+	                .when()
+	                    .post("/auth/login")
+	                .then()
+	                    .log().all()
+	                    .statusCode(200)
+	                    .body("token", not(empty()))
+	                    .extract().path("token");
+	    }
+}

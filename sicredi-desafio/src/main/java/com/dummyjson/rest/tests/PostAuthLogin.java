b679cf6b0f102f93d8f05f8696ad62be5c12c31b
Leandro Pereira <leandro.nares@gmail.com>
2024-08-22 11:02:11 -0300

    Refatoração do código

--- sicredi-desafio/src/main/java/com/dummyjson/rest/tests/PostAuthLogin.java
+++ sicredi-desafio/src/main/java/com/dummyjson/rest/tests/PostAuthLogin.java
@@ -6,13 +6,13 @@ import static org.hamcrest.Matchers.*;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import com.dummyjson.rest.core.BaseTest;
 
-public class PostAuthLogin extends BaseTest {
 
+public class PostAuthLogin extends BaseTest {
+	
 	private Map<String, String> createLoginPayload(String username, String password) {
 		Map<String, String> login = new HashMap<String, String>();
 		login.put("username", username);
@@ -20,23 +20,9 @@ public class PostAuthLogin extends BaseTest {
 		return login;
 	}
 
-	@Test
-	public void deveGerarTokenComSucesso() {
-		Map<String, String> login = createLoginPayload("emilys", "emilyspass");
-		given()
-			.body(login)
-		.when()
-			.post("/auth/login")
-		.then()
-			.log().all()
-			.statusCode(200)
-			.body("token", not(empty()))
-			.extract().path("token");
-	}
-
-	@Test
-	public void naoDeveGerarTokenSemUsername() {
-		Map<String, String> login = createLoginPayload("", "emilyspass");
+    @Test
+    public void naoDeveGerarTokenSemUsername() {
+    	Map<String, String> login = createLoginPayload("", "emilyspass");
 		given()
 			.body(login)
 		.when()
@@ -45,17 +31,18 @@ public class PostAuthLogin extends BaseTest {
 			.log().all()
 			.statusCode(400)
 			.body("message",is("Username and password required"));
-	}
+    }
 
-	@Test
-	public void naoDeveGerarTokenSemPassword() {
-		Map<String, String> login = createLoginPayload("emilys", "");
+    @Test
+    public void naoDeveGerarTokenSemPassword() {
+    	Map<String, String> login = createLoginPayload("emilys", "");
 		given()
 			.body(login)
 		.when()
 			.post("/auth/login")
-		.then().log().all()
+		.then()
+			.log().all()
 			.statusCode(400)
-			.body("message",Matchers.is("Username and password required"));
-	}
+			.body("message", is("Username and password required"));
+    }
 }
